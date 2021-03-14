5053960126877eed6e8d0e97c6539b091d6d08c8
Justin Mello <mello23@llnl.gov>
2021-03-14 11:54:33 -0700

    Adding logic for time.

--- hw2_1.cpp
+++ hw2_1.cpp
@@ -2,27 +2,59 @@
 #include <algorithm>
 #include <string>
 #include <ctime>
+#include <iomanip>
 
 using namespace std;
 int main() {
 
-
-
     string string1;
     string string2;
-    int result;
+    double result;
     cin >> string1 >> string2;
+
+    double hour1, hour2, min1, min2, sec1, sec2;
+    double hour3, min3, sec3;
+
 
     string1.erase(remove(string1.begin(), string1.end(), ':'), string1.end());
     string2.erase(remove(string2.begin(), string2.end(), ':'), string2.end());
 
-    double num1 = stoi(string1);
-    double num2 = stoi(string2);
+    hour1 = stod(string1.substr(0,2));
+    hour2 = stod(string2.substr(0,2));
+    min1 = stod(string1.substr(2,2));
+    min2 = stod(string2.substr(2,2));
+    sec1 = stod(string1.substr(4,2));
+    sec2 = stod(string2.substr(4,2));
 
-    result = difftime(num2,num1);
+//    double num1 = stod(string1);
+//    double num2 = stoi(string2);
+//    result = difftime(num2,num1);
 
-    cout << result;
+//    cout << result;
+
+    if(sec1 > sec2) {
+        min2++;
+        sec1 += 60;
+        sec3 = sec2 - sec1;
+    }
+    else
+        sec3 = sec2 - sec1;
+
+    if(min1 > min2) {
+        hour2++;
+        min1 += 1;
+        min3 = min2 - min1;
+    }
+    else {
+        min3 = min2 - min1;
+    }
+
+    hour3 = hour2 - hour1;
+
+
+    cout << setprecision(3) << hour3 << ":" << min3 << ":" << sec3;
     return 0;
+
 
 
 }
